631aceb79e911baf412b5ddf22531e2cdf0244ca
Hugo Geoffroy <batchimport@pstch.net>
2013-04-17 17:08:26 +0300

    Missing deletes

--- setup.py
+++ setup.py
@@ -12,17 +12,15 @@ from setuptools import setup
 setup(
     name='django-batchimport',
     version='0.1.2-pstch',
-    license='GPLv3',
     description='Django XLS batch processing',
     long_description="pistache's fork of Łukasz Mierzwa's project. Django template tags used to generate breadcrumbs html using twitter bootstrap css classes",
     author='Hugo Geoffroy',
     author_email='batchimport@pstch.net',
     packages = ['batchimport'],
     package_data = {
-        'batchimport': ['templates/*.html'],
+        'batchimport': ['templates/batchimport/*.html'],
     },
     classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
