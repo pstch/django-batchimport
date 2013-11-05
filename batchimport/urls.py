2a06a0b7b9448043331539415e5ba37b051664cb
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-05 07:17:32 +0100

    Add batchimport namespace

--- batchimport/urls.py
+++ batchimport/urls.py
@@ -5,7 +5,7 @@ If the default behavior of the batchimport views is acceptable to
 you, simply use a line like this in your root URLConf to set up the
 default URLs for batchimport:
 
-    (r'^batchimport/', include('batchimport.urls')),
+    (r'^batchimport/', include('batchimport.urls', namespace = 'batchimport')),
 
 But if you'd like to customize the behavior (e.g., by passing extra
 arguments to the various views) or split up the URLs, feel free to set
@@ -19,13 +19,13 @@ from django.conf.urls.defaults import *
 from batchimport.views import import_start, import_options, import_execute
 
 urlpatterns = patterns('',
-                       url(r'^form/$',
+                       url(r'^start/$',
                            import_start,
-                           name='batchimport_import_start'),
+                           name='start'),
                        url(r'^options/$',
                            import_options,
-                           name='batchimport_import_options'),
-                       url(r'^run/$',
+                           name='options'),
+                       url(r'^execute/$',
                            import_execute,
-                           name='batchimport_import_execute'),
+                           name='execute'),
 )
