b02b0ab9cf405772c6876c279826a5e60a51a36f
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-07 22:11:05 +0100

    Indent fix

--- batchimport/import.py
+++ batchimport/import.py
@@ -70,32 +70,33 @@ def _do_batch_import(request, model_import_info, book, sheet, process_option_dic
                         if process_option_dict['update_dupes']: 
                             for key in import_object_dict.keys():
                                 setattr(dupe_in_db, key, import_object_dict[key])
-                                dupe_in_db.save()
+                            dupe_in_db.save()
 
-                                status_msg = 'Updated : #%d' % str(row)
-                                status_dict['updated_count'] += 1
-                                if process_option_dict['show_successful_updates']:
-                                    status_dict['combined_messages'].append(status_msg)
-                                    status_dict['update_messages'].append(status_msg)
+                            status_msg = 'Updated : #%d' % str(row)
+                            status_dict['updated_count'] += 1
+                            if process_option_dict['show_successful_updates']:
+                                status_dict['combined_messages'].append(status_msg)
+                            status_dict['update_messages'].append(status_msg)
 
                     except ObjectDoesNotExist:
                         # The object doesn't exist. Go ahead and add it.
                         new_object = model_import_info.model_for_import(**import_object_dict)
                         new_object.save()
 
-                            status_msg = 'Imported : #%d'  %row 
-                            status_dict['imported_count'] += 1
-                            if process_option_dict['show_successful_imports']:
-                                status_dict['combined_messages'].append(status_msg)
-                                status_dict['import_messages'].append(status_msg)
-                            except:
-                                status_dict['error_messags'].append({'name' : 'Row processing error',
-                                                                     'critical' : 'No' if not process_option_dict['stop_on_first_error'] else "Yes",
-                                                                     'description' : str(sys.exc_info()[1]),
-                                                                     'info' : 'Row: %s' % row})
-                                if process_option_dict['stop_on_first_error']:
-                                    break
-                                    return status_dict
+                        status_msg = 'Imported : #%d'  %row 
+                        status_dict['imported_count'] += 1
+                        if process_option_dict['show_successful_imports']:
+                            status_dict['combined_messages'].append(status_msg)
+                        status_dict['import_messages'].append(status_msg)
+        except:
+            status_dict['error_messags'].append({'name' : 'Row processing error',
+                                                 'critical' : 'No' if not process_option_dict['stop_on_first_error'] else "Yes",
+                                                 'description' : str(sys.exc_info()[1]),
+                                                 'info' : 'Row: %s' % row})
+            if process_option_dict['stop_on_first_error']:
+                break
+                    
+    return status_dict
 
 
 def _do_relation_import(request, model_import_info, book, sheet, process_option_dict, status_dict):
@@ -147,29 +148,31 @@ def _do_relation_import(request, model_import_info, book, sheet, process_option_
                     cell_value = str(date_tuple[0]) + '-' + str(date_tuple[1])  + '-' + str(date_tuple[2])
                 else:
                     cell_value = cell.value
-                    row_value_list.append(cell_value)
+                row_value_list.append(cell_value)
 
-                    relationship_source_id_dict = model_import_info.get_relationship_source_id_dict(request, row_value_list)
-                    relationship_target_id_dict = model_import_info.get_relationship_target_id_dict(request, row_value_list)
+            relationship_source_id_dict = model_import_info.get_relationship_source_id_dict(request, row_value_list)
+            relationship_target_id_dict = model_import_info.get_relationship_target_id_dict(request, row_value_list)
 
-                    source_object = model_import_info.source_model.objects.get(**relationship_source_id_dict)
-                    target_object = model_import_info.target_model.objects.get(**relationship_target_id_dict)
+            source_object = model_import_info.source_model.objects.get(**relationship_source_id_dict)
+            target_object = model_import_info.target_model.objects.get(**relationship_target_id_dict)
 
-                    related_field_collection = getattr(source_object, model_import_info.mapping_field_name)
-                    related_field_collection.add(target_object)
+            related_field_collection = getattr(source_object, model_import_info.mapping_field_name)
+            related_field_collection.add(target_object)
 
-                    source_object.save()
+            source_object.save()
 
-                    status_msg = 'Updated : #%d' % str(row)
-                    status_dict['updated_count'] += 1
-                    if process_option_dict['show_successful_updates']:
-                        status_dict['combined_messages'].append(status_msg)
-                        status_dict['update_messages'].append(status_msg)
-                    except:
-                        status_dict['error_messags'].append({'name' : 'Row processing error',
-                                                             'critical' : 'No' if not process_option_dict['stop_on_first_error'] else "Yes",
-                                                             'description' : str(sys.exc_info()[1]),
-                                                             'info' : 'Row: %s' % row})
-                        if process_option_dict['stop_on_first_error']:
-                            break
-                            return status_dict
+            status_msg = 'Updated : #%d' % str(row)
+            status_dict['updated_count'] += 1
+            if process_option_dict['show_successful_updates']:
+                status_dict['combined_messages'].append(status_msg)
+            status_dict['update_messages'].append(status_msg)
+        except:
+            status_dict['error_messags'].append({'name' : 'Row processing error',
+                                                 'critical' : 'No' if not process_option_dict['stop_on_first_error'] else "Yes",
+                                                 'description' : str(sys.exc_info()[1]),
+                                                 'info' : 'Row: %s' % row})
+            if process_option_dict['stop_on_first_error']:
+                break
+
+    return status_dict
+
