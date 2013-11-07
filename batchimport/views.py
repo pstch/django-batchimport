a28f583def8ca9dffe4c7aafe393fbf6698f4b56
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-07 23:24:27 +0100

    test

--- batchimport/views.py
+++ batchimport/views.py
@@ -78,7 +78,9 @@ class ImportOptionsView(FormView):
                                             model_field_value_dict,
                                             form.relation_info_dict)
         
-        self.request.session['batchimport_info'] = model_import_info
+        info = { 'import_mode' : model_import_info.import_mode }
+
+        self.request.session['batchimport_info'] = info
         
         return render_to_response(self.processing_template_name)
 
